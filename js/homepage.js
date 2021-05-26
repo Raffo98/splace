39ac7e918ff421f064536ad3fdc0bb986fcae76e
Raffo98 <37075190+Raffo98@users.noreply.github.com>
2021-05-26 13:57:12 +0200

    Update logo resize

---
+++ js/homepage.js
@@ -0,0 +1,34 @@
+const logo = document.getElementsByClassName("logo")[0];
+const toggle = document.getElementsByClassName("toggle")[0];
+const menu = document.querySelector(".menu");
+
+// function toggleMenu() {
+//   if (menu.classList.contains("active")) {
+//     menu.classList.remove("active");
+//     toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+//   } else {
+//     menu.classList.add("active");
+//     toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+//   }
+// }
+//
+// toggle.addEventListener("click", toggleMenu, false);
+
+function logoreduced(x) {
+  if (x.matches) { // If media query matches
+    $(".logo").attr('src', "assets/logo/logo_red.png").on('load', function(){
+        logo.style.width = "28.8px";
+    });
+    // toggle.style.display = "block";
+  } else {
+    $(".logo").attr('src', "assets/logo/Logo_splace.png").on('load', function(){
+      logo.style.width = "120px";
+    });
+
+    // toggle.style.display = "none";
+  }
+}
+
+var x = window.matchMedia("(max-width: 750px)")
+logoreduced(x) // Call listener function at run time
+x.addListener(logoreduced) // Attach listener function on state changes
