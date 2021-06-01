16ad68eb322297b09dbc6cfc03578ac0ff5ea556
Raffo98 <37075190+Raffo98@users.noreply.github.com>
2021-06-01 16:43:19 +0200

    Update header

--- js/homepage.js
+++ js/homepage.js
@@ -12,9 +12,11 @@ toggleButton.addEventListener('click', function() {
 function toggleMenu() {
   if (menu.classList.contains("active")) {
     menu.classList.remove("active");
+    menu.style.height = "auto";
     toggleButton.innerHTML = "<i class='fas fa-bars'></i>";
   } else {
     menu.classList.add("active");
+    menu.style.height = "100vh";
     toggleButton.innerHTML = "<i class='fas fa-times'></i>";
   }
 }
