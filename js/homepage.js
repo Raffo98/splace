15decf4dcfe00030dae193b8ea40af725d6b9f55
ValerioAndrini <valerio.andrini@gmail.com>
2021-06-09 15:54:28 +0200

    Revert "Update map"
    This reverts commit 71aa0050a8f3fdd7032171756fc12707216ff1e5.

--- js/homepage.js
+++ js/homepage.js
@@ -1,9 +1,7 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoicmFmZmFlbGVyaWNjYXJkZWxsaSIsImEiOiJjazNpcmZoMzIwYWVzM2NwNm92eWpxbTdiIn0.15M15i30LdC8jUWkVsMSfg';
 var map = new mapboxgl.Map({
 container: 'map',
-style: 'mapbox://styles/raffaelericcardelli/ckpi7qoex04lt17t72gud5nuv',
-center: [9.189, 45.469],
-zoom: 11.67
+style: 'mapbox://styles/raffaelericcardelli/ckpi7qoex04lt17t72gud5nuv'
 });
 
 const toggleButton = document.getElementsByClassName('navbar-toggle')[0];
